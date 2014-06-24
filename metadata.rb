ba34b31df670dcda72fc5b7d63c4aaef115b3013
Lars Pfannenschmidt <lars@mobilecologne.de>
2014-06-24 17:47:06 +0200

    install swookiee via rpm from gh-pages

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,10 @@
 name             'swookiee-cookbook'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures swookiee-cookbook'
+maintainer       'larsp'
+maintainer_email 'lars@mobilecologne.de'
+license          'EPL'
+description      'Installs/Configures swookiee runtime'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.0.1'
+
+depends "java"
+depends "yum", "~> 3.0"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,14 @@
-#
-# Cookbook Name:: swookiee-cookbook
-# Recipe:: default
-#
-# Copyright 2014, Lars Pfannenschmidt
-#
-# All rights reserved - Do Not Redistribute
-#
+case node['platform_family']
+when 'rhel', 'fedora'
+
+    remote_file "#{Chef::Config[:file_cache_path]}/swookiee_latest.rpm" do
+    source "https://github.com/swookiee/swookiee-packages/blob/gh-pages/swookiee-1.0.0_SNAPSHOT-latest.noarch.rpm?raw=true"
+    action :create
+    end
+
+    rpm_package "swookiee" do
+        source "#{Chef::Config[:file_cache_path]}/swookiee_latest.rpm"
+        action :install
+    end
+
+end
