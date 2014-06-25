496a6d75a0fb3241b75aedbe3de4067bd23c67eb
Lars Pfannenschmidt <lars@mobilecologne.de>
2014-06-25 15:57:44 +0200

    Added basic configuration support

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default[:swookiee] = {
+    :gc => "-XX:+UseG1GC",
+    #In case you are not using not Java8
+    #:memory => "-XX:MaxPermSize=512m -Xms32m -Xmx1024m",
+    :memory => "-Xms32m -Xmx1024m",
+    :dropin_location => "/opt/swookiee/bundles"
+}
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,8 +2,8 @@ case node['platform_family']
 when 'rhel', 'fedora'
 
     remote_file "#{Chef::Config[:file_cache_path]}/swookiee_latest.rpm" do
-    source "https://github.com/swookiee/swookiee-packages/blob/gh-pages/swookiee-1.0.0_SNAPSHOT-latest.noarch.rpm?raw=true"
-    action :create
+        source "https://github.com/swookiee/swookiee-packages/blob/gh-pages/swookiee-1.0.0_SNAPSHOT-latest.noarch.rpm?raw=true"
+        action :create
     end
 
     rpm_package "swookiee" do
@@ -11,4 +11,17 @@ when 'rhel', 'fedora'
         action :install
     end
 
+end
+
+service "swookiee" do
+  supports :restart => true, :status => true
+  action [:enable, :start]
+end
+
+template ::File.join("/etc/default", "swookiee") do
+    source 'swookiee.erb'
+    owner 'swookiee'
+    group 'swookiee'
+    mode '644'
+    notifies :restart, resources(:service => "swookiee"), :delayed
 end
