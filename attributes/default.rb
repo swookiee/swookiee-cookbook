08d2c60abc7fde3fd38c590fe2a0b0d239e19b7b
Lars Pfannenschmidt <lars@mobilecologne.de>
2014-09-18 14:19:00 +0200

    moving to release v0.0.2 build 41 as default

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,6 @@ default[:swookiee] = {
     #:memory => "-XX:MaxPermSize=512m -Xms32m -Xmx1024m",
     :memory => "-Xms32m -Xmx1024m",
     :dropin_location => "/opt/swookiee/bundles",
-    :version => "0.0.1",
-    :build => "39"
+    :version => "0.0.2",
+    :build => "41"
 }
