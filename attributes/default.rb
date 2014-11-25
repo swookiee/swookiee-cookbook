7cb4a4cba6c62212f9c9a4e723f5a7ccfdaef9ed
Lars Pfannenschmidt <lars@mobilecologne.de>
2014-11-25 14:40:48 +0100

    remove GC and MEMORY and added JVM_OPTS instead moving to build 47

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,8 @@
 default[:swookiee] = {
-    :gc => "-XX:+UseG1GC",
-    #In case you are not using not Java8
-    #:memory => "-XX:MaxPermSize=512m -Xms32m -Xmx1024m",
-    :memory => "-Xms32m -Xmx1024m",
+    # In case you are not using Java8
+    #:jvm_opts => "-XX:MaxPermSize=512m -Xms32m -Xmx1024m",
+    :jvm_opts => "-XX:+UseG1GC -Xms32m -Xmx1024m",
     :dropin_location => "/opt/swookiee/bundles",
     :version => "0.0.4",
-    :build => "44"
+    :build => "47"
 }
