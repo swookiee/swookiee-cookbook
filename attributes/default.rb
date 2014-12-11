2a566bbd0541f4c3fe6bc444f5f5b2359a4ad56c
Lars Pfannenschmidt <larsp@users.noreply.github.com>
2014-12-11 14:11:47 +0100

    Update to swookiee 0.0.6 with rpm build 1.0.3

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,6 @@ default[:swookiee] = {
     #:jvm_opts => "-XX:MaxPermSize=512m -Xms32m -Xmx1024m",
     :jvm_opts => "-XX:+UseG1GC -Xms32m -Xmx1024m",
     :dropin_location => "/opt/swookiee/bundles",
-    :version => "0.0.4",
-    :rpm_version => "1.0.0"
+    :version => "0.0.6",
+    :rpm_version => "1.0.3"
 }
