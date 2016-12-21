6dafe964af7517758dba83bc2f5f1e2deb0c4dd5
Tobias Wenzel <tobiaswenzel123@gmail.com>
2016-12-21 11:58:40 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'lars@mobilecologne.de'
 license          'EPL'
 description      'Installs/Configures swookiee runtime'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.2'
+version          '0.0.3'
 
 depends "java"
 depends "yum", "~> 3.0"
