eaabf35a1e28e59146243d55a68011b897125982
Lars Pfannenschmidt <lars@d8a.wtf>
2017-01-06 08:26:20 -0800

    Updated default swookiee version

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,6 @@ default[:swookiee] = {
     :jvm_opts => "-XX:+UseG1GC -Xms32m -Xmx1024m",
     :dropin_location => "/opt/swookiee/bundles",
     :process_priority => "0",
-    :version => "0.1.0",
-    :rpm_version => "1.0.16"
+    :version => "0.1.1",
+    :rpm_version => "1.0.17"
 }
